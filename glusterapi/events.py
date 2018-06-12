dce3f495129f5e49ca2ba2ca901377972e5b11c5
Madhu Rajanna <mrajanna@redhat.com>
2018-06-12 16:53:20 +0530

    Implemeted client event api's for glusterd2
    Signed-off-by: Madhu Rajanna <mrajanna@redhat.com>

---
+++ glusterapi/events.py
@@ -0,0 +1,45 @@
+import httplib
+import json
+
+from glusterapi.common import BaseAPI
+
+
+class EventsApis(BaseAPI):
+    def webhook_add(self, url="", token="", secret=""):
+        """
+        Gluster add webhook.
+
+        :param url: (string) url to add webhook
+        :param token: (string) token for webhook
+        :param secret: (string) secret for webhook
+        :raises: GlusterApiError  on failure
+        """
+        req = {
+            "url": url,
+            "token": token,
+            "secret": secret
+        }
+        return self._handle_request(self._post, httplib.OK,
+                                    "/v1/events/webhook", json.dumps(req))
+
+    def webhook_delete(self, url=""):
+        """
+        Gluster delete webhook.
+
+        :param url: (string) url to delete webhook
+        :raises: GlusterApiError on failure
+        """
+        req = {
+            "url": url,
+        }
+        return self._handle_request(self._delete, httplib.NO_CONTENT,
+                                    "/v1/events/webhook", json.dumps(req))
+
+    def webhooks(self):
+        """
+        Gluster list all webhooks.
+
+        :raises: GlusterApiError on failure
+        """
+        return self._handle_request(self._get, httplib.OK,
+                                    "/v1/events/webhook")
